264f809f3785ac43152652b742f019242773acae
Anand Pradeep Kumar Sharma <asharm13@vm-hw04.eecs.tufts.edu>
2023-02-25 21:12:27 -0500

    solution for hw11

--- array/array.cpp
+++ array/array.cpp
@@ -1,4 +1,124 @@
 #include "array.h"
+#include <cstdlib> // For malloc, free
+#include <cstring> // For memcpy
+#include <iostream>
 
-// Not much to say here.  Good luck!
+Array::Array() : values(nullptr), size(0), capacity(0) {}
 
+Array::Array(int length, int value)  {
+    allocate(length);
+    size = length;
+    for (int i = 0; i < length; i++) {
+        values[i] = value;
+    }
+}
+
+Array::Array(int length, int* values){
+    allocate(length);
+    size = length;
+    for(int i = 0; i < length; i++){
+	    this->values[i] = values[i];
+    }
+}
+
+Array::Array(int length){
+	allocate (length);
+	size = length;
+	for(int i = 0; i < length; i++){
+		this->values[i] = 0;
+	}
+}
+
+
+Array::Array(const Array& other){
+    copy(other);
+}
+
+Array::~Array() {
+    delete[] values;
+}
+Array& Array::operator=(const Array& other){
+	if(this == & other){
+		return *this;
+	}
+
+	copy(other);
+	return *this;
+}
+
+
+int Array::getSize() const {
+    return size;
+}
+
+void Array::append(int x) {
+    if (size == capacity) {
+        int* newValues = new int[capacity * 2];
+	for(int i = 0; i < capacity; i++){
+		newValues[i] = values[i];
+	}
+delete[] values;
+values = newValues;
+capacity *= 2;
+    }
+    values[size] = x;
+    size++;
+}
+
+void Array::append(const Array& other) {
+    for (int i = 0; i < other.size; i++) {
+        append(other.values[i]);
+    }
+}
+
+Array Array::operator+(const Array& other){
+    Array result(size + other.size);
+    for (int i = 0; i<size; i++){
+	    result.values[i] = values[i];
+    }
+    for (int i = 0; i<other.size; i++){
+	    result.values[size + i] = other.values[i];
+    }
+
+      return result;
+}
+
+
+int& Array::operator[](int index) {
+	if (index < 0 || index >= size){
+		std::cout << "error" << std::endl;
+		exit(0);
+}
+return values[index];
+}
+const int& Array::operator[](int index) const{
+//int Array::operator[](int index) cont{
+	if(index < 0 || index >= size){
+		std::cout << "error" << std::endl;
+		exit(0);
+	}
+    return values[index];
+}
+
+std::ostream& operator<<(std::ostream& stream, const Array& array) {
+    stream << "[";
+    for (int i = 0; i < array.size; i++) {
+        stream << array.values[i];
+        if (i < array.size - 1) {
+            stream << ", ";
+        }
+    }
+    stream << "]";
+    return stream;
+}
+
+//void Array::resize(int newCapacity) {
+  //  int* newData = (int*) realloc(data, newCapacity * sizeof(int));
+    //if (newData == nullptr) {
+      //  std::cerr << "Failed to allocate memory." << std::endl;
+        //exit(1);
+   // }
+    //data = newData;
+   // capacity = newCapacity;
+    //length = std::min(length, capacity);
+//}
--- array/test_array.cpp
+++ array/test_array.cpp
@@ -1,31 +1,90 @@
 #include "array.h"
 #include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-  // Here's some quick test code to get you started.  You'll need much more
-  // thorough testing, of course.
+  // Create an array of length 3 with initial value 0
+  //Array a(3, 0);
 
-  Array arr;
-  arr.initialize(10, 5);
+  // Append some values to the array
+  //a.append(1);
+ // a.append(2);
+  //a.append(3);
 
-  Array rtoo;
-  rtoo.initialize(0, 0);
-  rtoo = arr;
+  // Print the modified array
+ // std::cout << "a = " << a << std::endl;
 
-  for(int i = 0; i < arr.size(); i++){
-    arr[i] = i * 2;
-  }
+  // Create another array of length 3 with initial value 0
+  //Array b(3, 0);
+
+  // Append some values to the array
+ // b.append(4);
+ // b.append(5);
+  //b.append(6);
+
+  // Print the modified array
+  //std::cout << "b = " << b << std::endl;
+
+  // Concatenate the two arrays
+  //Array c = a + b;
+
+  // Print the concatenated array
+  //std::cout << "a + b = " << c << std::endl;
+
+  // Create another array with the same values as a
+  //Array d = a;
+
+  // Change a value in the new array
+  //d[0] = 9;
+
+  // Print the modified array
+  //std::cout << "d = " << d << std::endl;
+
+  // Print the original array
+ // std::cout << "a = " << a << std::endl;
+
+  // Print the value at index 1 in the array
+  //std::cout << "a[1] = " << a[1] << std::endl;
+
+  // Print the value at an invalid index in the array
+ // std::cout << "a[10] = " << a[10] << std::endl;
+ Array a;
+
+    // Create an array with length and initial value
+    Array b(5, 10);
+
+    // Create an array with length and values
+    int data[] = {1, 2, 3, 4, 5};
+    Array c(5, data);
+
+    // Copy an array
+    Array d = c;
+
+    // Modify an element of the array using bracket notation
+    d[0] = 10;
+
+    // Append an element to the end of an array
+    d.append(6);
+
+    // Append an array to another array
+    d.append(b);
+
+    // Concatenate two arrays
+    Array e = c + d;
+
+    // Print the size of an array
+    cout << "Size of array e: " << e.getSize() << endl;
+
+    // Print the content of an array using the << operator
+    cout << "Array e: " << e << endl;
+
+    // Test out of bounds error
+    e[100] = 1;
+
+  return 0;
 
 
-  for(int i = 0; i < arr.size(); i++){
-    printf(" arr[%d] = %d\n", i, arr[i]);
-  }
-
-  for(int i = 0; i < rtoo.size(); i++){
-    printf("rtoo[%d] = %d\n", i, rtoo[i]);
-  }
-
-  return(0);
 }
 
