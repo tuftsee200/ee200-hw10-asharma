ad8fe6cd3d97db750fd1ee79daa3d99089d9d088
Anand Pradeep Kumar Sharma <asharm13@vm-hw09.eecs.tufts.edu>
2023-10-20 18:31:44 -0400

    Test20/10

--- array/test_array.cpp
+++ array/test_array.cpp
@@ -7,51 +7,7 @@
 #include <iostream>
 #include <cstring>
 
-// Test our Array class
-int main(int argc, char* argv[])
-{
-    // Test the default constructor
-    Array<int> arr;
-    arr.append(3);
-    arr.append(4);
-    arr.append(5);
 
-    // Test the copy constructor
-    Array<int> copy(arr);
-    copy[1] = 6;
-
-    // Test the = operator
-    Array<int> copy2;
-    copy2 = arr;
-    copy2[1] = 7;
-
-    // Test + operator
-    Array<int> sum = arr + copy + copy2;
-
-    // Test the [] operator
-    for (int i = 0; i < sum.getSize(); i++)
-    {
-        std::cout << sum[i] << ' ';
-    }
-
-    std::cout << std::endl;
-
-    // Test out of bounds error
-    try
-    {
-        std::cout << sum[sum.getSize()] << std::endl;
-    }
-    catch (const std::exception& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    // Test the << operator
-    // std::cout << sum << std::endl;
-
-    return 0;
-}
-/*
 #include <exception>
 
 
@@ -94,4 +50,4 @@ int main(int argc, char* argv[]) {
     }
     return 0;
 }
-*/
+
